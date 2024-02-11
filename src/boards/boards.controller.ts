30063151bb3d3b8f14c183b75a7777d1f2b89ec1
재국 <jk6722@hanyang.ac.kr>
2024-02-11 14:43:47 +0900

    feat: 게시물 생성시 요청값 유효성 검사

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -6,6 +6,8 @@ import {
   Param,
   Patch,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 import { Board, BoardStatus } from './board.model';
@@ -23,6 +25,7 @@ export class BoardsController {
 
   //게시물 생성
   @Post()
+  @UsePipes(ValidationPipe)
   createBoard(@Body() createBoardDto: CreateBoardDto): Board {
     return this.boardsService.createBoard(createBoardDto);
   }
