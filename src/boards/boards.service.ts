7234cd1360f1ff5c7a3d55398559cadfe3e43d1a
재국 <jk6722@hanyang.ac.kr>
2024-02-11 13:08:34 +0900

    feat: 게시물 생성을 위한 DTO 생성

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './\bdto/request/create-board.dto';
 
 @Injectable()
 export class BoardsService {
@@ -12,7 +13,8 @@ export class BoardsService {
   }
 
   //게시물 생성
-  createBoard(title: string, description: string): Board {
+  createBoard(createBoardDto: CreateBoardDto): Board {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(),
       title,
