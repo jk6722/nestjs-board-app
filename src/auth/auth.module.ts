03bae2227c93d5c3d9085174c63b41c66bd3f110
재국 <jk6722@hanyang.ac.kr>
2024-02-14 00:31:14 +0900

    chore: JwtStrategy 사용을 위한 모듈 설정

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,10 +3,25 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './user.entity';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.register({
+      secret: 'Secret Text',
+      signOptions: {
+        expiresIn: 60 * 60, // 토큰 유효시간 (1시간)
+      },
+    }),
+    TypeOrmModule.forFeature([User]),
+  ],
   controllers: [AuthController],
-  providers: [AuthService],
+  //JwtStrategy를 이 Auth 모듈에서 사용할 수 있도록 import
+  providers: [AuthService, JwtStrategy],
+  //JwtStrategy, PassportModule을 다른 모듈에서 사용할 수 있도록 등록
+  exports: [JwtStrategy, PassportModule],
 })
 export class AuthModule {}
