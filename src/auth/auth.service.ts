3c985e52732302dde6b1255c45bf92e894b3b294
재국 <jk6722@hanyang.ac.kr>
2024-02-14 00:30:12 +0900

    feat: 로그인 성공시 jwt 토큰 생성

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -9,12 +9,14 @@ import { User } from './user.entity';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialDto } from './dto/request/auth-credential.dto';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   //회원 생성
@@ -42,13 +44,18 @@ export class AuthService {
   }
 
   //로그인
-  async signIn(authCredentialDto: AuthCredentialDto): Promise<string> {
+  async signIn(
+    authCredentialDto: AuthCredentialDto,
+  ): Promise<{ accessToken: string }> {
     const { username, password } = authCredentialDto;
     const user = await this.userRepository.findOneBy({ username });
 
     if (user && (await bcrypt.compare(password, user.password))) {
       //암호 일치(로그인 성공)
-      return 'login success';
+      //유저 토큰 생성
+      const payload = { username }; //페이로드로는 사용자 이름, 이메일 등 다양한 정보를 넣을 수 있다. 단, 중요한 정보는 넣지 않도록 한다.
+      const accessToken = await this.jwtService.sign(payload); // (secret + payload)
+      return { accessToken };
     } else {
       throw new UnauthorizedException('login failed');
     }
