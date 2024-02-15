cea0e1e7e4bcf72a48f98a2555500a316fd5cfd8
재국 <jk6722@hanyang.ac.kr>
2024-02-15 21:08:12 +0900

    feat: 자신의 게시물에만 접근할 수 있는 권한 처리

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -17,35 +17,43 @@ import { CreateBoardDto } from './\bdto/request/create-board.dto';
 import { BoardStatusValidationPipe } from 'src/boards/pipes/board-status-validation.pipe';
 import { Board } from './board.entity';
 import { AuthGuard } from '@nestjs/passport';
+import { GetUser } from 'src/auth/get-user.decorator';
+import { User } from 'src/auth/user.entity';
 
 @Controller('boards')
 @UseGuards(AuthGuard()) // 컨트롤러 단에서 동작
 export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
-  //모든 게시물 조회
+  //유저의 모든 게시물 조회
   @Get()
-  getAllBoards(): Promise<Board[]> {
-    return this.boardsService.getAllBoards();
+  getAllBoards(@GetUser() user: User): Promise<Board[]> {
+    return this.boardsService.getAllBoards(user);
   }
 
   //게시물 생성
   @Post()
   @UsePipes(ValidationPipe)
-  createBoard(@Body() createBoardDto: CreateBoardDto): Promise<Board> {
-    return this.boardsService.createBoard(createBoardDto);
+  createBoard(
+    @Body() createBoardDto: CreateBoardDto,
+    @GetUser() user: User,
+  ): Promise<Board> {
+    return this.boardsService.createBoard(createBoardDto, user);
   }
 
   //게시물 단건 조회
   @Get('/:id')
-  getBoardById(@Param('id') id: number): Promise<Board> {
-    return this.boardsService.getBoardById(id);
+  getBoardById(@Param('id') id: number, @GetUser() user: User): Promise<Board> {
+    return this.boardsService.getBoardById(id, user);
   }
 
   //게시물 삭제
   @Delete('/:id')
-  deleteBoardById(@Param('id', ParseIntPipe) id: number): Promise<void> {
-    return this.boardsService.deleteBoardById(id);
+  deleteBoardById(
+    @Param('id', ParseIntPipe) id: number,
+    @GetUser() user: User,
+  ): Promise<void> {
+    return this.boardsService.deleteBoardById(id, user);
   }
 
   //특정 게시물 상태 업데이트
@@ -53,7 +61,8 @@ export class BoardsController {
   updateBoardStatus(
     @Param('id', ParseIntPipe) id: number,
     @Body('status', BoardStatusValidationPipe) status: BoardStatus,
+    @GetUser() user: User,
   ): Promise<Board> {
-    return this.boardsService.updateBoardStatus(id, status);
+    return this.boardsService.updateBoardStatus(id, status, user);
   }
 }
--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -4,6 +4,7 @@ import { CreateBoardDto } from './\bdto/request/create-board.dto';
 import { Board } from './board.entity';
 import { Repository } from 'typeorm';
 import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/auth/user.entity';
 
 @Injectable()
 export class BoardsService {
@@ -12,19 +13,28 @@ export class BoardsService {
     private readonly boardRepository: Repository<Board>,
   ) {}
 
-  //모든 게시물 조회
-  async getAllBoards(): Promise<Board[]> {
-    return this.boardRepository.find();
+  //유저의 모든 게시물 조회
+  async getAllBoards(user: User): Promise<Board[]> {
+    const query = this.boardRepository.createQueryBuilder('board');
+
+    query.where('board.userId = :userId', { userId: user.id });
+
+    const boards = await query.getMany();
+    return boards;
   }
 
   //게시물 생성
-  async createBoard(createBoardDto: CreateBoardDto): Promise<Board> {
+  async createBoard(
+    createBoardDto: CreateBoardDto,
+    user: User,
+  ): Promise<Board> {
     const { title, description } = createBoardDto;
 
     const board = this.boardRepository.create({
       title,
       description,
       status: BoardStatus.PUBLIC,
+      user,
     });
 
     await this.boardRepository.save(board);
@@ -32,8 +42,8 @@ export class BoardsService {
   }
 
   //특정 게시물 조회
-  async getBoardById(id: number): Promise<Board> {
-    const findBoard = await this.boardRepository.findOneBy({ id });
+  async getBoardById(id: number, user: User): Promise<Board> {
+    const findBoard = await this.boardRepository.findOneBy({ id, user });
 
     if (!findBoard)
       throw new NotFoundException(`Can't find Board with id ${id}`);
@@ -42,16 +52,21 @@ export class BoardsService {
   }
 
   //특정 게시물 삭제
-  async deleteBoardById(id: number): Promise<void> {
-    const result = await this.boardRepository.delete(id);
+  async deleteBoardById(id: number, user: User): Promise<void> {
+    //id와 user 정보 모두 일치하는 경우 삭제
+    const result = await this.boardRepository.delete({ id, user });
     if (result.affected === 0) {
       throw new NotFoundException(`Can't find board with id ${id}`);
     }
   }
 
   //특정 게시물 상태 업데이트
-  async updateBoardStatus(id: number, status: BoardStatus): Promise<Board> {
-    const findBoard = await this.getBoardById(id);
+  async updateBoardStatus(
+    id: number,
+    status: BoardStatus,
+    user: User,
+  ): Promise<Board> {
+    const findBoard = await this.getBoardById(id, user);
 
     findBoard.status = status;
     this.boardRepository.save(findBoard);
